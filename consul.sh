927201f43bb29c0d8591575c659e299eea87e008
Ryan Shaw <ryan.shaw@min.vc>
2016-03-15 23:04:51 +0000

    Create docker-machine for consul

--- consul.sh
+++ consul.sh
@@ -1,4 +1,11 @@
+#!/bin/bash -e
+
+MACHINE=$1
+
+docker-machine create -d generic --generic-ip-address=$MACHINE
+
 docker run -d \
 	-p "8500:8500" \
 	-h "consul" \
+	--restart=always \
 	progrium/consul -server -bootstrap
