57d4fc39baf37353a11a96349d9beac99a3251c1
Ryan Shaw <ryan.shaw@min.vc>
2016-03-15 22:39:52 +0000

    Add multi-host networking

---
+++ master_box.sh
@@ -0,0 +1,1 @@
+docker run -it --rm --net=overlay_network --env="constraint:node==c00-master-01" busybox wget -O- http://web
---
+++ nginx_app01.sh
@@ -0,0 +1,2 @@
+docker run -itd --name=web --net=overlay_network --env="constraint:node==c00-app-01" nginx
+
